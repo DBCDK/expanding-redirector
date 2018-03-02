e3966dff7952cb0825c2dbab21866868e815db59
Morten Bøgeskov <mb@dbc.dk>
2018-03-02 10:50:35 +0100

    Added docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+shopt -s nullglob
+
+if [ -d /docker-entrypoint-confd.d ]; then
+	for script in /docker-entrypoint-confd.d/*.sh; do
+    	echo "===> Executing confd script " $script
+		$script
+	done
+fi
+
+python3 /var/lib/redirector/expanding-redirector.py -d /var/lib/redirector/ -p $PORT
